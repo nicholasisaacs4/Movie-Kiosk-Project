a99a01b52d982559241764a130fd7d20d6d6b57d
beckylindquist <beckylindquist2018@gmail.com>
2022-03-31 15:46:06 -0400

    added objects to movie class

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -1,4 +1,18 @@
 
 public class Movie {
-    
+    String title;
+    String genre;
+    String director;
+    int IDNum;
+    double price;
+    int ranking;
+    dateTime dateRented;
+    dateTime dateDue;
+    dateTime dateReturned;
+
+    public Movie(String title, String director, String genre){
+        this.title=title;
+        this.genre=genre;
+        this.director=director;
+    }
 }
