c97a9146830321096c3bcd348a66368ceecc94a0
beckylindquist <beckylindquist2018@gmail.com>
2022-04-01 11:29:51 -0400

    Added get/set IDNum and get/set Price methods

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -16,4 +16,28 @@ public class Movie {
         this.genre=genre;
         this.director=director;
     }
+
+    int getIDNum(){
+        return IDNum;
+    }
+
+    void setIDNum(){
+        //get IDNum of last movie and add 1 to it
+    }
+
+    double getPrice(){
+        return price;
+    }
+
+    void setPrice(){
+        if(ranking>=10){ //if rented >10 times
+            price = 20;
+        }
+        else if(ranking>1 && ranking<10){ //if rented between 2-10 times
+            price = 10;
+        }
+        else if(ranking<=1){ //if rented 0 or 1 time
+            price = 5;
+        }
+    }
 }
