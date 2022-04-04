d65a3d6a87e903f16b658d26b563711fcdd06cb3
beckylindquist <beckylindquist2018@gmail.com>
2022-04-04 11:26:54 -0400

    changed names of objects in library, added library test parameters

--- src/test/libraryTest.java
+++ src/test/libraryTest.java
@@ -1,8 +1,14 @@
 package test;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.time.LocalDate;
 
 public class libraryTest {
-    
+    public libraryTest(){
+        @Test
+        void sortByRankTest(){
+            
+        }
+    }
 }
